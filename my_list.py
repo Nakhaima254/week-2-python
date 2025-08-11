deb7cc6d7c8ec90d0a0e456fd4c85987073bd8f4
Nakhaima254 <nakhaimaisaac068gmail.com>
2025-08-11 14:46:48 +0300

    Initial Commit

---
+++ my_list.py
@@ -0,0 +1,11 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)  # Insert 25 at index 1
+my_list.extend([50, 60, 70])  # Extend the list with multiple values
+my_list.pop()  # Remove the last item from the list
+my_list.sort()  # Sort the list in ascending order
+
+print(my_list[3])  # Print the item at index 3
